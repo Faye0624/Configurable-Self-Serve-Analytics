2f4f574dfa6b32dcca0544b0549b11fdc3aefd28
Faye0624 <15302602073@163.com>
2026-07-20 18:48:53 +0100

    expose classes via package __init__

--- ssa/models/__init__.py
+++ ssa/models/__init__.py
@@ -1,1 +1,7 @@
 # Domain model: plain data objects (Project, DatasetTable, Column, Role).
+from ssa.models.role import Role
+from ssa.models.column import Column
+from ssa.models.dataset_table import DatasetTable
+from ssa.models.project import Project
+
+__all__ = ["Role", "Column", "DatasetTable", "Project"]
